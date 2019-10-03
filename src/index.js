95592634d53c414b013bf9e3a0f1d5c9c92451af
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2019-10-03 16:58:04 -0300

    Removed the fixed PORT to deploy with heroku

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const { SERVER_PORT = "3000" } = process.env;
+const { PORT } = process.env;
 
 const app = express();
 
@@ -15,4 +15,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require("./app/controllers/authController")(app);
 require("./app/controllers/transactionController")(app);
 
-app.listen(SERVER_PORT);
+app.listen(PORT | 3000);
